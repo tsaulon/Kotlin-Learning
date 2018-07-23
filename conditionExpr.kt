b9da84b74accc4310080f762e7dba543883ea989
Tyrone <tyrone.saulon@hotmail.com>
2018-07-23 17:57:36 -0400

    Adding conditional expressions

---
+++ conditionExpr.kt
@@ -0,0 +1,22 @@
+//If Statements Implementation
+
+fun main(args: Array<String>){
+
+    var x = 10
+    var y = 20
+
+    //  Conventional Declaration
+    if(max(x, y)){
+        println("$x is greater than $y");
+    } else{
+        println("$x is not greater than $y");
+    }
+
+    //  Ternary Operations (Kind of...)
+    println(if(max(++x, ++y)) "$x is greater than $y" else "$x is not greater than $y")
+
+}
+
+fun max(a: Int, b: Int): Boolean {
+    return a > b
+}
