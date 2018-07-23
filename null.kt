8622db48fe35d222366fae18ee8efde44f4e2dd7
Tyrone <tyrone.saulon@hotmail.com>
2018-07-23 18:26:38 -0400

    Adding null values

---
+++ null.kt
@@ -0,0 +1,25 @@
+//  Handling Null Datatypes
+fun main(args: Array<String>){
+    printProduct("12", "15")    //  180
+    printProduct("Ham", "Eggs") //  java.lang.NumberFormatException: For input string: "Ham"
+}
+
+//  Return null if str does not hold an integer
+fun parseInt(str: String) : Int?{
+    return str.toInt();
+}
+
+//  Example:
+fun printProduct(a: String, b: String){
+
+    try {
+        val x = parseInt(a)
+        val y = parseInt(b)
+
+        if(x !== null && y !== null)
+        println(x * y)
+
+    } catch(e: Exception){
+        println(e.message);
+    }
+}
