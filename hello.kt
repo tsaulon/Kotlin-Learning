f7e8f92dd8e0575b4ac92084a709ff2efb15578c
Tyrone <tyrone.saulon@hotmail.com>
2018-07-23 16:12:10 -0400

    Greeting from Main

---
+++ hello.kt
@@ -0,0 +1,9 @@
+//  Compiling on Windows CLI:
+//  >   Set Windows environment variables to point to Kotlin compiler
+//  >   More details @ https://kotlinlang.org/docs/tutorials/command-line.html
+//  >   kotlinc filename.kt -include-runtime -d filename.jar
+//  >   java -jar filename.jar
+
+fun main(args: Array<String>){
+    println("It is now the Kotlin uprising!");
+}
