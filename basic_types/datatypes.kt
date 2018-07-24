38bf6bef83966a4835550c6cb454238a6307dcda
Tyrone <tyrone.saulon@hotmail.com>
2018-07-24 00:53:50 -0400

    Adding more datatypes

--- basic_types/datatypes.kt
+++ basic_types/datatypes.kt
@@ -1,21 +1,25 @@
+import com.sun.org.apache.xpath.internal.operations.Bool
+
 fun main(args: Array<String>){
 
-    //  Datatypes
+    //  DATATYPES
     //  Integer values refer to  it's bit width
 
     val double: Double = 64.0
     val float: Float = 32.0F  //  requires trailing 'F'
-    var long: Long = 64L   //  Optional but good practice needs trailing 'L'
+    val long: Long = 64L   //  Optional but good practice needs trailing 'L'
     val integer: Int = 32
     val short: Short = 16
     val byte: Byte = 8
+    val char: Char
+    val boolean: Boolean
 
-    //  Numeric Literals
+    //  NUMERIC LITERALS
     //  can use '_' to make numbers more readable
     val oneMillion: Int? = 1_000_000
     val creditCardNumber: Long? = 1234_5678_9012_3456L
 
-    //  Representation
+    //  REPRESENTATION
     val a: Int = 10_000
     println(a === a) // true
     val boxedA: Int? = a                //  Int? signifies nullable reference value
