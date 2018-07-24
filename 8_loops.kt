a291581f335d62cc16a2bc0ac0e3cc79365f5522
Tyrone <tyrone.saulon@hotmail.com>
2018-07-23 21:38:37 -0400

    Adding For and While loops

---
+++ 8_loops.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>){
+
+    //  for loops
+    val items = listOf("apple", "banana", "kiwifruit")
+    var itemStr: String = "For: "
+    for(item in items){
+        itemStr += " $item${if(item != items.last()) ", " else ""}"
+    }
+    println(itemStr)
+
+    //  while loops
+    itemStr = "While: " //  reset
+    var index = 0
+    while(index < items.size){
+       itemStr += "[$index]${items[index]}${if(items[index++] !== items.last()) ", " else ""}"
+    }
+    println(itemStr)
+
+}
