5f0eb6e6940dbdb039dd000a4509563ebf40eaac
Tyrone <tyrone.saulon@hotmail.com>
2018-07-25 01:48:14 -0400

    Adding expression labels, breaks, and continues

--- returns_jumps/1_breakContinueLabels.kt
+++ returns_jumps/1_breakContinueLabels.kt
@@ -6,12 +6,48 @@ fun main(args: Array<String>){
     //  -   return
     //  -   break
     //  -   continue
+
+    fun foo(something: Boolean?){
+        val s = something ?: return //  return if the parameter is null
+    }
+
+    //  Labels - Expression Identifiers
+    foo@ for(i in 1..100){
+        if(i === 2) {
+            println("Stopped at 2 label@foo")
+            break@foo   //  or 'continue'
+        }
+    }
+
+    println("Iterations")
+    foo()
+    println()
+    bar()
+    println()
+    baz()
+
 }
 
-fun foo(something: Boolean) : Any{
-    val s = something ?: return 0
-    return s
+//  Explicitly calling Lambda Expression's identifier
+fun foo(){
+    return listOf(1, 2, 3, 4, 5).forEach lit@{
+        if(it == 3) return@lit
+        print("$it ")
+    }
 }
 
-//  TODO: Continue reading 'Returns and Jumps' from 'Basics' in Kotlin documentation
-//  https://kotlinlang.org/docs/reference/returns.html
+//  Implicitly calling the Lambda Expression of the calling function (forEach)
+fun bar(){
+    return listOf(1, 2, 3, 4, 5).forEach{
+        if(it == 3) return@forEach
+        print("$it ")
+    }
+}
+
+//  JS Anonymous function styles
+fun baz(){
+    listOf(1, 2, 3, 4, 5).forEach(fun(value: Int){
+        if(value == 3) return
+        print("$value ")
+    })
+}
